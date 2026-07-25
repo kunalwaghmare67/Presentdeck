827b6124cd134bc809b0b9adefd98c9442a443c7
kunalwaghmare67 <kunalwaghmare2005@gmail.com>
2026-07-25 22:52:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        presenting: resolve(__dirname, 'presenting.html'),
+        presenting: resolve(__dirname, 'index.html'),
       },
     },
   },
